1b1363595e082e16feca50e43b797013c1e90284
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2019-01-31 22:38:37 +0100

    Confirm symlink if link already exists

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 lns() {
     mkdir -p "$(dirname "$HOME/$1")"
-    ln -sv "$PWD/$1" "$HOME/$1"
+    if [[ $(readlink "$HOME/$1") = "$PWD/$1" ]]; then
+        echo $1 already symlinked
+        return
+    fi
+    ln -isv "$PWD/$1" "$HOME/$1"
 }
 
 for f in $(git ls-files); do
